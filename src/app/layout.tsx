5964ccf38020f87b4db8fffa0128de5d4326a011
saku-1101 <sakupi1101@outlook.jp>
2023-07-03 11:46:08 +0900

    update: セッション切れてなければログイン不要にした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,37 @@ import './globals.css';
 import './index.css';
 import { Inter } from 'next/font/google';
 import { store } from '@/redux/rootStore';
-import { Header } from '../components/molecules/Header/Header';
+import { useState, useEffect } from 'react';
+import Login from './login/pages';
+import { supabase } from '../../supabase';
+import { Session } from '@supabase/supabase-js';
+import Home from './home/page';
 
 const inter = Inter({ subsets: ['latin'] });
 
 console.log(store.getState());
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    // async () => {
+    //   const { data } = await supabase.auth.getSession();
+    //   setSession(data.session);
+    // };
+    supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event, session);
+      setSession(session);
+    });
+    // return () => {
+    //   authListener.unsubscribe();
+    // };
+  }, []);
+
   return (
-    <Provider store={store}>
-      <html lang='en'>
-        <Header user={{ name: 'Jane Doe' }} />
-        <body className={inter.className}>{children}</body>
-      </html>
-    </Provider>
+    <html lang='en'>
+      <Provider store={store}>
+        <body className={inter.className}>{session ? <Home /> : <Login />}</body>
+      </Provider>
+    </html>
   );
 }
