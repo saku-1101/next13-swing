ef5676bf7aa86576e71b4206d2dab4efbf7ee8d2
saku-1101 <sakupi1101@outlook.jp>
2023-07-04 12:06:31 +0900

    update: login, register切り替え

--- src/components/atoms/LoginForm/index.tsx
+++ src/components/atoms/LoginForm/index.tsx
@@ -1,18 +1,8 @@
-// import classnames from 'classnames';
 import { useAppDispatch } from '@/redux/hooks/hooks';
-import { updatePetState, updateUserState } from '@/redux/slices';
-import { User } from '@/redux/slices/userSlice';
-import { redirect } from 'next/navigation';
+import { updatePetState } from '@/redux/slices';
 import React, { useState } from 'react';
 import { supabase } from '../../../../supabase';
 import { AuthError } from '@supabase/supabase-js';
-
-// mock data
-const user: User = {
-  name: 'Sakura A',
-  uuid: '1',
-  password: 'sakuranopassword',
-};
 
 export function LoginForm() {
   const [isLogin, setIsLogin] = useState(false);
@@ -51,33 +41,33 @@ export function LoginForm() {
         alert(error.message);
       }
     }
-    // console.log(formData);
-    // dispatch(updatePetState(sentence));
-    // dispatch(updateUserState(user));
-    // // redirect(`/home/${userid}`)
-    // redirect('/home');
-    // setIsLogin(true);
+    dispatch(updatePetState(sentence));
   };
   return (
     <>
-      <div className='w-full flex items-center justify-center'>
+      <div className='w-screen h-screen flex items-center justify-center'>
         <div className='bg-white shadow-md rounded w-1/3'>
-          <div className='px-4 py-3 bg-blue-500'>
-            <h3 className='text-lg font-semibold'>{isLogin ? 'Login' : 'Register'}</h3>
+          <div className='px-4 py-3 rounded bg-accent'>
+            <h3 className='text-lg text-white font-semibold'>{isLogin ? 'Login' : 'Register'}</h3>
           </div>
-          {isLogin ? (
-            <p>
-              If you wish to Register &gt;&gt;{' '}
-              <span onClick={() => setIsLogin(false)}>Switch to Registration mode</span>
-            </p>
-          ) : (
-            <p>
-              If you wish to Login &gt;&gt; <span onClick={() => setIsLogin(true)}>Switch to Login mode</span>
-            </p>
-          )}
           <div className='modal-body p-4'>
+            {isLogin ? (
+              <p className='text-sm text-zinc-500'>
+                If you wish to Register &gt;&gt;{' '}
+                <span className='text-accent' onClick={() => setIsLogin(false)}>
+                  Switch to Registration mode
+                </span>
+              </p>
+            ) : (
+              <p className='text-sm text-zinc-500'>
+                If you wish to Login &gt;&gt;{' '}
+                <span className='text-accent' onClick={() => setIsLogin(true)}>
+                  Switch to Login mode
+                </span>
+              </p>
+            )}
             <form onSubmit={handleAuth}>
-              <div className='mb-4'>
+              <div className='mt-4 mb-4'>
                 <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2'>
                   Email:
                 </label>
@@ -87,7 +77,7 @@ export function LoginForm() {
                   required
                   id='email'
                   className='block w-full p-2 border-2 border-blue-500 rounded'
-                  placeholder='Enter your email'
+                  placeholder='Email...'
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
@@ -119,7 +109,7 @@ export function LoginForm() {
                   required
                   id='password'
                   className='block w-full p-2 border-2 border-blue-500 rounded'
-                  placeholder='Enter your password'
+                  placeholder='Passoword...'
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
@@ -132,12 +122,12 @@ export function LoginForm() {
                   id='pet'
                   value={sentence}
                   className='block w-full p-2 border-2 border-blue-500 rounded'
-                  placeholder='I want to have cats!'
+                  placeholder='E.g, I want to have cats!'
                   onChange={(e) => setSentence(e.target.value)}
                 />
               </div>
               <div className='flex justify-end'>
-                <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+                <button type='submit' className='bg-accent hover:bg-tahiti text-white font-bold py-2 px-4 rounded'>
                   {isLogin ? 'Login' : 'Register'}
                 </button>
               </div>
